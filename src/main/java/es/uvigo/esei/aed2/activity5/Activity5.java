112d65f93cad7264a0c66e49433362e7dc248932
Icey <99903003+Icey-e621@users.noreply.github.com>
2025-11-18 13:00:14 +0100

    Change nullCheck return value from -1 to 0

--- src/main/java/es/uvigo/esei/aed2/activity5/Activity5.java
+++ src/main/java/es/uvigo/esei/aed2/activity5/Activity5.java
@@ -94,14 +94,14 @@ public class Activity5 {
 
   //exercise 6
   public static <T> int getGrade(Tree<T> tree) {
-    if (nullCheck(tree)) return -1;
+    if (nullCheck(tree)) return 0;
     if (!tree.hasChildren() && !tree.hasRightSibling()) return 1;
     return Math.max(1+getGrade(tree.getRightSibling()), getGrade(tree.getLeftMostChild()));
   }
 
   //exercise 7
   public static <T> int getHeight(Tree<T> tree) {
-    if (nullCheck(tree)) return -1;
+    if (nullCheck(tree)) return 0;
     if (!tree.hasChildren() && !tree.hasRightSibling()) return 0;
     return Math.max(1+getHeight(tree.getLeftMostChild()), getHeight(tree.getRightSibling()));
   }
